a1e3de74593970f53812d37640abc2e57a76291c
Liam <98386642+liam-ob@users.noreply.github.com>
2024-06-12 09:32:52 +0800

    added important entries

--- diary/entries/models.py
+++ diary/entries/models.py
@@ -12,6 +12,7 @@ class Entry(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     date_created = models.DateTimeField(default=timezone.now())
+    important = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
